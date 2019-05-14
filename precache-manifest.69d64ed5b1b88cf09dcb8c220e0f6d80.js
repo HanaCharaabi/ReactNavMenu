38c41bb294f2eb305bdd69c9ffdd9dfc2ff931df
hana Charaabi <charaabihana90@gmail.com>
2019-05-14 08:45:34 +0100

    Updates

---
+++ precache-manifest.69d64ed5b1b88cf09dcb8c220e0f6d80.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "18eb7b067cbbe39a5121b62d6cd5493e",
+    "url": "/ReactNavMenu/index.html"
+  },
+  {
+    "revision": "bbb374ce0686b4494b6f",
+    "url": "/ReactNavMenu/static/css/main.236d0981.chunk.css"
+  },
+  {
+    "revision": "9eee67cf782a38d35aeb",
+    "url": "/ReactNavMenu/static/js/2.d6a534e5.chunk.js"
+  },
+  {
+    "revision": "bbb374ce0686b4494b6f",
+    "url": "/ReactNavMenu/static/js/main.11411e66.chunk.js"
+  },
+  {
+    "revision": "d7a3cfc399af15e1975a",
+    "url": "/ReactNavMenu/static/js/runtime~main.0d4eee78.js"
+  }
+]);
